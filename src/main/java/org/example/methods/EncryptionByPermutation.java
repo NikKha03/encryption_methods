b2ed15a6e63843ef2942530fb0a0eb72798666d2
NikKha03 <kolyakhalimendik@gmail.com>
2024-04-18 16:44:34 +0300

    update

--- src/main/java/org/example/methods/EncryptionByPermutation.java
+++ src/main/java/org/example/methods/EncryptionByPermutation.java
@@ -73,4 +73,13 @@ public class EncryptionByPermutation implements Method {
 
         return MatrixUtils.columInText(columns, consistency, matrixWight, matrixHeight);
     }
+
+    public static void printCharArray(char[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
